4ad71a4520c8be03544c2d688dfea371f6b5a404
icam <wisyam_zain_31rpl@smktelkom-mlg.sch.id>
2024-03-23 22:41:15 +0700

    update

--- routes/car.route.js
+++ routes/car.route.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const authControll = require('../controllers/auth.controller')
 const roleValidate = require('../middleware/role.validation')
 
-app.get('/', authControll.authorize, carController.getAllCar)
+app.get('/', carController.getAllCar)
 app.get('/:key', authControll.authorize, carController.findCar)
 app.post('/', authControll.authorize, roleValidate.isAdmin, carController.addCar)
 app.put('/:id', authControll.authorize, roleValidate.isAdmin, carController.updateCar)
